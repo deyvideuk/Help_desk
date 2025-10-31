704effd9083d0b42b72c1f6ad61216d964e0ed45
deyvideuk <deyvideukcosta.dc@gmail.com>
2025-10-31 13:45:32 -0300

    Feat: implementação de formulario de newsletter, função de realizar cadastro de emails implementada

--- src/app/componentes/paginas/home/home.ts
+++ src/app/componentes/paginas/home/home.ts
@@ -2,11 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { Header } from '../../layout/header/header';
 import { Footer } from '../../layout/footer/footer';
+import { FormControl, FormGroup, FormGroupName, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
   imports: [
     RouterOutlet,
+    ReactiveFormsModule,
     Header,
     Footer
   ],
@@ -14,5 +16,14 @@ import { Footer } from '../../layout/footer/footer';
   styleUrl: './home.css'
 })
 export class Home {
+ newsletter = new FormGroup({
+    email : new FormControl("", [Validators.email, Validators.required])
+ });
+
+ onSubmitNewsletter(){
+    if(this.newsletter.valid){
+      alert("Cadastro realizado com sucesso para: " + this.newsletter.controls.email.value);
+    }
+ }
 
 }
