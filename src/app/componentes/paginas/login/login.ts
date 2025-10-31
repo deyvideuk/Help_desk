6b950ea0fca9c6a3416133449ba9c05b0ef16ec0
c <antoniocesarcostademelo@gmail.com>
2025-10-31 12:26:44 -0300

    test: alteração para form implementando o formGroup no ts

--- src/app/componentes/paginas/login/login.ts
+++ src/app/componentes/paginas/login/login.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './login.html',
   styleUrl: './login.css'
 })
 export class Login {
-
+  form = new FormGroup({
+    cpf : new FormControl(''),
+    senha : new FormControl('')
+  });
 }
