e725a19c67f985492da3f7c990255ef2459f439e
deyvideuk <deyvideukcosta.dc@gmail.com>
2025-10-31 11:10:36 -0300

    feat: implementou um novo botão no menu e adicionou uma ancora entre o botão de entrar no menu para a página de login

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { Home } from './componentes/paginas/home/home';
+import { Login } from './componentes/paginas/login/login';
 
 export const routes: Routes = [
     {
         path: "",
         component: Home
+    },
+    {
+        path: "login",
+        component: Login
     }
 ];
