dab308fac0e0d6c48c452b33154636d9539c73ef
deyvideuk <deyvideukcosta.dc@gmail.com>
2025-11-01 00:12:37 -0300

    Style: implementação de versão mobile, estilização e melhorias de CSS refactor: Otimização do menu para diferentes telas desktop/mobile

--- src/app/componentes/layout/header/header.ts
+++ src/app/componentes/layout/header/header.ts
@@ -1,12 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Injectable } from '@angular/core';
 import { RouterLink } from "@angular/router";
+import { MenuMobile } from '../menu-mobile/menu-mobile';
 
 @Component({
   selector: 'app-header',
-  imports: [RouterLink],
+  imports: [
+    RouterLink,
+    MenuMobile
+  ],
   templateUrl: './header.html',
   styleUrl: './header.css'
 })
-export class Header {
 
+@Injectable({
+  providedIn: 'root'
+})
+
+export class Header {
+  width = window.innerWidth;
+  
+  
+  public menu(opt:string){
+    if(opt == 'open'){
+      let menu = document.querySelector('#header') as HTMLElement;
+      let btnMenu = document.querySelector('.menu') as HTMLElement;
+
+      menu.style.left = "0";
+      btnMenu.style.display = "none";
+    }else if(opt == 'close'){
+      let menu = document.querySelector('#header') as HTMLElement;
+      let btnMenu = document.querySelector('.menu') as HTMLElement;
+
+      menu.style.left = "-700px";
+      btnMenu.style.display = "flex";
+    }
+  }
 }
---
+++ src/app/componentes/layout/menu-mobile/menu-mobile.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { Header } from '../header/header';
+
+
+@Component({
+  selector: 'app-menu-mobile',
+  imports: [Header],
+  templateUrl: './menu-mobile.html',
+  styleUrl: './menu-mobile.css'
+})
+export class MenuMobile {
+
+}
