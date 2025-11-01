94f77e29bcce99c29e7e97667fe6f2cdcfb64ebb
deyvideuk <deyvideukcosta.dc@gmail.com>
2025-11-01 14:34:37 -0300

    Style: Página de Login ajustada

--- src/app/componentes/layout/menu-desktop/menu-desktop.ts
+++ src/app/componentes/layout/menu-desktop/menu-desktop.ts
@@ -1,8 +1,11 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-menu-desktop',
-  imports: [],
+  imports: [
+    RouterLink
+  ],
   templateUrl: './menu-desktop.html',
   styleUrl: './menu-desktop.css'
 })
